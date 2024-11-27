759d7570c3f18fca914811a1449832092b5d7373
Alop18 <104959094+Alop18@users.noreply.github.com>
2024-11-26 20:49:17 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+const { nextui } = require("@nextui-org/react");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('/src/assets/malteadas.jpg')"
+      },
+      colors: {
+        'bg-malteadas': '#040404'
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()]
+}
+
